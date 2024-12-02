84e9b5f336afd1789d7964a940ba1586bf7ff606
andresbasilea <andresbasilealvarez@gmail.com>
2024-12-01 23:41:21 -0600

    working Kafka streaming pipeline and ksql listening to like changes

--- constants.py
+++ constants.py
@@ -4,7 +4,7 @@ import os
 
 
 parser = configparser.ConfigParser()
-parser.read(os.path.join(os.path.dirname(__file__), 'config/config.local'))
+parser.read(os.path.join(os.path.dirname(__file__), 'config.local'))
 
 YOUTUBE_API_KEY = parser.get('youtube', 'API_KEY')
 PLAYLIST_ID = parser.get('youtube', 'PLAYLIST_ID')
--- youtube_analytics.py
+++ youtube_analytics.py
@@ -1,21 +1,24 @@
-import requests
-from pprint import pprint
-from constants import YOUTUBE_API_KEY, PLAYLIST_ID
 import json
-from kafka import KafkaProducer
 import logging
 
+import requests
+from kafka import KafkaProducer
+
+from constants import YOUTUBE_API_KEY, PLAYLIST_ID
+
+
 def fetch_page(url, parameters, page_token=None):
-    params = {**parameters, 'key': YOUTUBE_API_KEY, 'page_token':page_token}
+    params = {**parameters, 'key': YOUTUBE_API_KEY, 'page_token': page_token}
     response = requests.get(url, params)
     payload = json.loads(response.text)
-    #logging.info("Response = %s", payload)
 
     return payload
+
 
 def fetch_page_lists(url, parameters, page_token=None):
     while True:
         payload = fetch_page(url, parameters, page_token)
+        #print(payload)
         yield from payload['items']
 
         page_token = payload.get('nextPageToken')
@@ -28,45 +31,12 @@ def format_response(video):
         'title': video['snippet']['title'],
         'likes': int(video['statistics'].get('likeCount', 0)),
         'comments': int(video['statistics'].get('commentCount', 0)),
-        'views': int(video['statistics'].get('viewCount', 0))
-        }
+        'views': int(video['statistics'].get('viewCount', 0)),
+        'favorites': int(video['statistics'].get('favoriteCount', 0)),
+        'thumbnail': video['snippet']['thumbnails']['default']['url']
+    }
     return video_res
 
-
-
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
-    # producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
-
-    # response = requests.get("https://www.googleapis.com/youtube/v3/playlistItems", 
-    #                         {
-    #                             'key': YOUTUBE_API_KEY   ,
-    #                             'id': 'piJkuavhV50',
-    #                             'part': 'snippet, statistics, status'
-    #                         })
-    
-    # #print(pprint(response.text))
-    # response = json.loads(response.text)['items']
-    # for video in response:
-    #     video_res = {
-    #     'title': video['snippet']['title'],
-    #     'likes': int(video['statistics'].get('likeCount', 0)),
-    #     'comments': int(video['statistics'].get('commentCount', 0)),
-    #     'views': int(video['statistics'].get('viewCount', 0))
-    #     }
-    #     print(video_res)
-        
-    #     producer.send('youtube_videos', json.dumps(video_res).encode('utf-8'))
-    # producer.flush()
-
-    # response = requests.get("https://www.googleapis.com/youtube/v3/playlistItems", 
-    #                         {
-    #                             'key': YOUTUBE_API_KEY   ,
-    #                             'playlistId': PLAYLIST_ID,
-    #                             'part': 'snippet, contentDetails, status',
-    #                             'page_token': 'EAAajgFQVDpDQVVpRURsRk9ERTBORUV6TlRCR05EUXdPRUlvQVVpWDZwSDM3c3lKQTFBQldrVWlRMmxLVVZSR2NFbFZWVGxwVkRGa1ZWVlZVbEZWTUhSMVpFWldhbFJWUm5sU01taHNaVlpPVGs1SFpFMU9NMlJVUldkM1NUbExjVFIxVVZsUk1reFFVRE5SU1NJ'
-    #                         })
-    # print(response.text)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
@@ -86,5 +56,7 @@ if __name__ == "__main__":
             # logging.info("Video here => %s", pprint(format_response(video)))
             producer.send('youtube_videos', json.dumps(format_response(video)).encode('utf-8'),
                           key=video_id.encode('utf-8'))
-            print('Sent ', video['snippet']['title'])
+            print('Sent to topic ', video['snippet']['title'])
             # producer.flush()
+
+
